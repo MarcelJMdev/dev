ccb6f49e89bb03bc2718a509d0316799bec4b003
MarcelJMdev <marceljm376@gmail.com>
2025-12-13 14:07:00 +0100

    Update downloads.php

--- downloads.php
+++ downloads.php
@@ -1,39 +1,27 @@
 <?php
-$plugin = $_GET['plugin'] ?? null;
+// Fehler anzeigen (nur zum Debuggen, später entfernen)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-if (!$plugin) {
-    http_response_code(400);
-    exit("Plugin fehlt");
-}
-
-// FESTER Dateiname (dein echtes Plugin)
 $fileName = "StatsPlugin-1.0.0.jar";
 $filePath = __DIR__ . "/plugins/" . $fileName;
-$dataFile = __DIR__ . "/downloads.json";
 
 if (!file_exists($filePath)) {
     http_response_code(404);
-    exit("Datei nicht gefunden");
+    echo "DATEI NICHT GEFUNDEN: " . $filePath;
+    exit;
 }
 
-// JSON laden
-$data = json_decode(file_get_contents($dataFile), true);
-if (!is_array($data)) {
-    $data = [];
-}
+// Sauberer Download für alle Browser
+header('Content-Description: File Transfer');
+header('Content-Type: application/java-archive');
+header('Content-Disposition: attachment; filename="' . $fileName . '"');
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($filePath));
 
-// Zähler erhöhen
-if (!isset($data[$plugin])) {
-    $data[$plugin] = 0;
-}
-$data[$plugin]++;
-
-// Speichern
-file_put_contents($dataFile, json_encode($data, JSON_PRETTY_PRINT));
-
-// Datei ausliefern
-header("Content-Type: application/java-archive");
-header("Content-Disposition: attachment; filename=\"$fileName\"");
-header("Content-Length: " . filesize($filePath));
+flush();
 readfile($filePath);
 exit;
